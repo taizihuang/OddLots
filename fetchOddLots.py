475280b15ca589f14baf2c3a7f00d0a3ba4a02ad
Taizi Huang <htzllt@gmail.com>
2021-12-18 00:31:44 +0800

    Update fetchOddLots.py

--- fetchOddLots.py
+++ fetchOddLots.py
@@ -10,9 +10,13 @@ def fetchOddLots(url,title):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
     docs = requests.get(url,headers=headers).content
     doc = BeautifulSoup(docs)
-    doc1 = BeautifulSoup(json.loads(doc.find('script',{'data-component-props':"ArticleBody"}).string)['body'])
-    if doc1.find(class_="thirdparty-embed__container"):
-        doc1.find(class_="thirdparty-embed__container").decompose()
+    body = json.loads(doc.find('script',{'data-component-props':"ArticleBody"}).string)
+    if body:
+        doc1 = BeautifulSoup(body['body'])
+        if doc1.find(class_="thirdparty-embed__container"):
+            doc1.find(class_="thirdparty-embed__container").decompose()
+    else:
+        doc1 = ''
     html = '''
     <!DOCTYPE html>
     <html>
