be0e9bfbb2540414645f90f7b0edb87872ecac69
Taizi Huang <htzllt@gmail.com>
2023-05-16 16:38:47 +0800

    1

--- fetchOddLots.py
+++ fetchOddLots.py
@@ -1,23 +1,20 @@
 import requests,os,json
 import pandas as pd
 from bs4 import BeautifulSoup
+
 # os.environ['https_proxy'] = 'http://127.0.0.1:7890'
 # os.environ['http_proxy'] = 'http://127.0.0.1:7890'
 
+headers = {
+    'referer': 'https://www.bloomberg.com/oddlots',
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+}
 
 def fetchOddLots(url,title):
-    headers = {
-        'referer': 'https://www.bloomberg.com/oddlots',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
     docs = requests.get(url,headers=headers).content
-    doc = BeautifulSoup(docs,features="lxml")
-    body = json.loads(doc.find('script',{'data-component-props':"LeaderboardAd"}).string)
-    if 'story' in body:
-        doc1 = BeautifulSoup(body['story']['body'],features="lxml")
-        if doc1.find(class_="thirdparty-embed__container"):
-            doc1.find(class_="thirdparty-embed__container").decompose()
-    else:
-        doc1 = ''
+    doc = BeautifulSoup(docs)
+    doc1 = BeautifulSoup(json.loads(doc.find('script',{'data-component-props':"ArticleBody"}).string)['story']['body'])
+    doc1.find(class_="thirdparty-embed__container").decompose()
     html = '''
     <!DOCTYPE html>
     <html>
@@ -37,13 +34,16 @@ def fetchOddLots(url,title):
     '''.format(title=title)
     html = html+str(doc1)+'</body></html>'
 
+    df_odd = pd.DataFrame(columns=['date','title','link','content'])
     date = url.split('/')[5]
-    df_odd = pd.DataFrame(data={'date':date,'title':title,'link':url,'content':html},index=[0])
+    df_odd = df_odd.append({'date':date,'title':title,'link':url,'content':html},ignore_index=True)
 
     return df_odd
 
 def genHTML(df_odd):
 
+    df_odd = df_odd.sort_values('date',ascending=False)
+    
     item = ''
     for idx in df_odd.index:
         date = df_odd.loc[idx,'date']
@@ -51,7 +51,7 @@ def genHTML(df_odd):
         html = df_odd.loc[idx,'content']
         title = df_odd.loc[idx,'title']
         name = link.split('/')[-1].split('?')[0]
-        item = item+"""<ul class="LI"><li><a class="title" href="./html/{name}.html">[{date}] {title}</a></li></ul>""".format(date=date, name=name,title=title)
+        item = item+"""<ul class="LI"><li><a class="title" href="./html/{name}.html">[{date}] {title}</a></li></ul>""".format(date=date,name=name,title=title)
         with open('./html/{}.html'.format(name),'w',encoding='utf8') as f:
             f.write(html)
 
@@ -77,22 +77,12 @@ def genHTML(df_odd):
     with open('./index.html','w',encoding='utf8') as f:
         f.write(indexHTML)
 
-url = 'https://www.bloomberg.com/oddlots'
-headers = {
-    'referer': 'https://www.bloomberg.com/oddlots',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
-doc = BeautifulSoup(requests.get(url,headers=headers).content, features="lxml")
-
 df_odd = pd.read_pickle('oddlots.pkl')
-
-for item in doc.findAll(class_='story-list-story__info__headline-link'):
-    if 'Transcript' in item.text:
-        title = item.text
-        print(title)
-        link = 'https://www.bloomberg.com' + item['href']
-        df_odd_new = fetchOddLots(link,title=title)
-        df_odd = pd.concat([df_odd_new,df_odd],ignore_index=True)
-        df_odd = df_odd.drop_duplicates(['title'])
-df_odd = df_odd.sort_values(by=['date'],ascending=False)        
+doc = requests.get('https://www.bloomberg.com/lineup/api/lazy_load_paginated_module?id=more_articles_list&offset=0&page=oddlots&zone=switch',headers=headers).content
+link_title = list(set([(a['href'], a.text) for a in BeautifulSoup(json.loads(doc)['html']).findAll('a') if 'transcript' in a['href'] if '\n\n' not in a.text]))
+for link, title in link_title:
+    if len(df_odd.loc[df_odd.title.str.contains(title)]) == 0:
+        df = fetchOddLots('https://www.bloomberg.com'+link, title)
+        df_odd = pd.concat([df, df_odd], ignore_index=True)
+        df_odd.to_pickle('oddlots.pkl')
 genHTML(df_odd)
-df_odd.to_pickle('oddlots.pkl')
